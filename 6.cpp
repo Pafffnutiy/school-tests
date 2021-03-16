841c23f3db3c796c681a9be04870252c7f8837cb
Lord <lord.nemo@protonmail.com>
2021-03-16 14:56:38 +0300

    6 partial solution added

--- 6.cpp
+++ 6.cpp
@@ -6,8 +6,14 @@ int main()
     std::vector<long long int> First, Second{-5, 6}, Ans;
     for (long long int inp; std::cin >> inp;)
         First.push_back(inp);
-    Ans.push_back(First[0] * Second[0]);
-    for (long long int i = 1; i < First.size(); ++i)
+    Ans.resize(First.size() + Second.size() - 1);
+    for (auto &elem : Ans)
+        elem = 0;
+    for (long long int i = 0; i < First.size(); ++i)
+        for(long long int j = 0; j < Second.size(); ++j)   
+            Ans[i + j] += First[i] * Second[j];
 
+    for(auto elem : Ans)
+        std::cout << elem << ' ';
     return 0;
 }
