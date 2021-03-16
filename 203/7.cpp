55cd68d09bb31d40779e0eb6158286030f12d638
Lord <lord.nemo@protonmail.com>
2021-03-16 20:59:26 +0300

    added 202 folder

--- 203/7.cpp
+++ 203/7.cpp
@@ -5,14 +5,15 @@ const int32_t INF = 1215752192;
 
 int main() //TODO test this shit
 {
+    /*
     std::fstream w("input");
     for(int32_t i = -5; i < 5; ++i)
         w.write(reinterpret_cast<char*>(&i), 4);
     w.close();
-
+    */
 
     std::fstream file("input");
-    int32_t first, second, temp, q=4, cnt = 0, k = 0, m = INF; // k - the number of numbers; m - position to place the sum (after m-th number)
+    int32_t temp, q=4, cnt = 0, k = 0, m = INF; // k - the number of numbers; m - position to place the sum (after m-th number)
     for (int32_t i = 0, tmp; !file.eof(); ++i){
         ++k;
         file.read(reinterpret_cast<char*>(&tmp), q);
